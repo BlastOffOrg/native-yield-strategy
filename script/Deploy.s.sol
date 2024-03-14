44d4f254bf21c87366ff7eb998450f4568e9fa51
DuyAnhTran95 <duyanh.tran8995@gmail.com>
2024-03-14 12:11:08 +0700

    deploy native yield script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -24,6 +24,36 @@ contract Deploy is Script {
         );
 
         // Pick an unique salt
+        bytes32 salt = keccak256("v3.0.2");
+
+        address contractAddress = deployer.deployCreate2(salt, bytecode);
+
+        console.log("Address is ", contractAddress);
+
+        vm.stopBroadcast();
+    }
+}
+
+contract DeployNativeYieldStrategy is Script {
+// Create X address.
+    Deployer public deployer =
+        Deployer(0xba5Ed099633D3B313e4D5F7bdc1305d3c28ba5Ed);
+
+    // Vault factory address for v1.0.0
+    // TODO: change this after deploying factory
+    address public factory = 0xc187547c4C8beF4907B86b8a7e0AC400F5c1Cb94;
+
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        // Append constructor args to the bytecode
+        bytes memory bytecode = abi.encodePacked(
+            vm.getCode("NativeYieldStrategy.sol:TokenizedStrategy"),
+            abi.encode(factory)
+        );
+
+        // Pick an unique salt
         bytes32 salt = keccak256("Native Yield Strategy v1.0.0");
 
         address contractAddress = deployer.deployCreate2(salt, bytecode);
